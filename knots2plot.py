88118a81c8cb12fa36a098cea3db6da0ae7c994e
adam <adam.narozniak@gmail.com>
2016-04-30 16:02:13 +0200

    added direction arrows to paths with toggle functionality

--- knots2plot.py
+++ knots2plot.py
@@ -167,8 +167,10 @@ def gcodexyuv(dataxy, datauv):
 
 
 lines = []
+arrows_list=[]
 col_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-arrow_length = 30
+al = 30
+hl=al*0.5
 parser = argparse.ArgumentParser(description='plot knots')
 parser.add_argument('-i',
                     '--input',
@@ -203,50 +205,59 @@ for input_file in input_file_list:
         line, = ax.plot(x1, y1, lw=2, marker=plt_marker,
                         color=col_list[i], label=input_file)
         lines.append(line)
+
         seg_len = ((x1[1] - x1[0])**2 + (y1[1] - y1[0])**2)**0.5
-        len_rat = arrow_length / seg_len
+        len_rat = al / seg_len
         x2 = x1[0] + (x1[1] - x1[0]) * len_rat
         y2 = y1[0] + (y1[1] - y1[0]) * len_rat
-        ax.annotate('({0:.2f},{1:.2f})'.format(x1[0], y1[0]),
-                    xy=(x2, y2),
-                    xytext=(x1[0], y1[0]),
-                    arrowprops=dict(fc=col_list[i], ec=col_list[
-                                    i], width=4, frac=0.1, shrink=0.1),
-                    fontsize=6,
-                    horizontalalignment='center',
-                    verticalalignment='center',)
-# head_width=0.05, head_length=0.1,
+        arrow = ax.annotate('({0:.2f},{1:.2f})'.format(x1[0], y1[0]),
+                             xy=(x2, y2),
+                             xytext=(x1[0], y1[0]),
+                             arrowprops=dict(fc=col_list[i], ec=col_list[i], width=4, headlength=hl, shrink=0.1),
+                             fontsize=6,
+                             horizontalalignment='center',
+                             verticalalignment='center',)
+        arrows_list.append(arrow)
+# move to the next color
         if i == len(col_list) - 1:
             i = 0
         else:
             i += 1
 
-        # arrows1.append(ax.plot(x1[0], y1[0],lw=1, marker='s',color='r',ms=10)[0])
-        # arrows2.append(ax.plot(x1[1], y1[1],lw=1, marker='*',color='r',ms=10)[0])
-        # for no, (x, y) in enumerate(zip(x1,y1)):
-        #     # print('{0} ({1}, {2})'.format(i,x,y))
-        # ax.text(x,y,'{0}'.format(no+1,x,y),fontsize=6) # ({1:.2f}, {2:.2f})
-
 # legend display
 leg = ax.legend(loc='upper left')
 leg.get_frame().set_alpha(0.4)
 # legend display
+print lines
+print arrows_list
+
+# draw()
+#plt.pause(5)
+
+arrows_list[1].set_visible(False)
 
 lined = dict()
-
+arrowed = dict()
 
 for legline, origline in zip(leg.get_lines(), lines):
     legline.set_picker(5)  # 7 pts tolerance
     lined[legline] = origline
 
+for legline, origarrow in zip(leg.get_lines(), arrows_list):
+    arrowed[legline] = origarrow
 
 def onpick(event):
     # on the pick event, find the orig line corresponding to the
     # legend proxy line, and toggle the visibility
+
     legline = event.artist
+
     origline = lined[legline]
+    origarrow = arrowed[legline]
     vis = not origline.get_visible()
     origline.set_visible(vis)
+    origarrow.set_visible(vis)
+
     # Change the alpha on the line in the legend so we can see what lines
     # have been toggled
     if vis:
