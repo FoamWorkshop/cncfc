604f416ea29a78624f97229e9372b9b03c04749e
adam <adam.narozniak@gmail.com>
2016-04-30 16:50:55 +0200

    added knots numbers to plot, changed line styles

--- knots2plot.py
+++ knots2plot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-'''the purpose of the program is to generate 3d model for FreeCAD from 2 given sections'''
+'''the purpose of the program is to visualise knot sets'''
 # import os
 # import sys
 import numpy as np
@@ -15,10 +15,13 @@ from scipy.spatial import distance
 
 
 lines = []
-arrows_list=[]
-col_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+arrows_list = []
+coords_list = []
+coords_no_list = []
+col_list = ['b', 'g', 'r', 'c', 'm', 'y', '0.75']
+styl_list = ['-', '-', ':', ':', '--', '--', '-.']
 al = 30
-hl=al*0.5
+hl = al * 0.5
 parser = argparse.ArgumentParser(description='plot knots')
 parser.add_argument('-i',
                     '--input',
@@ -49,23 +52,36 @@ for input_file in input_file_list:
     if data:
         x1 = [var[0] for var in data]
         y1 = [var[1] for var in data]
-
+# plot paths
         line, = ax.plot(x1, y1, lw=2, marker=plt_marker,
-                        color=col_list[i], label=input_file)
+                        color=col_list[i], label=input_file, linestyle=styl_list[i])
         lines.append(line)
 
+# plot direction arrows
         seg_len = ((x1[1] - x1[0])**2 + (y1[1] - y1[0])**2)**0.5
         len_rat = al / seg_len
         x2 = x1[0] + (x1[1] - x1[0]) * len_rat
         y2 = y1[0] + (y1[1] - y1[0]) * len_rat
-        arrow = ax.annotate('({0:.2f},{1:.2f})'.format(x1[0], y1[0]),
-                             xy=(x2, y2),
-                             xytext=(x1[0], y1[0]),
-                             arrowprops=dict(fc=col_list[i], ec=col_list[i], width=4, headlength=hl, shrink=0.1),
-                             fontsize=6,
-                             horizontalalignment='center',
-                             verticalalignment='center',)
+        #({0:.2f},{1:.2f})
+        arrow = ax.annotate(''.format(x1[0], y1[0]),
+                            xy=(x2, y2),
+                            xytext=(x1[0], y1[0]),
+                            arrowprops=dict(fc=col_list[i], ec=col_list[
+                                            i], width=4, headlength=hl, shrink=0.1),
+                            fontsize=6,
+                            horizontalalignment='center',
+                            verticalalignment='center',)
         arrows_list.append(arrow)
+# plot knot numbers and coordinates
+        for no, var in enumerate(zip(x1, y1)):
+
+            coord_no = ax.annotate('{0}'.format(no), xy=(var[0], var[1]), xytext=(var[0], var[1]),fontsize=6,horizontalalignment='center',
+                verticalalignment='center',)
+
+            # coord = ax.annotate('({0:.2f}, {1:.2f})'.format(
+            #     var[0], var[1]), xy=(var[0], var[1]), xytext=(var[0], var[1]),fontsize=6,horizontalalignment='center',
+            #     verticalalignment='center',)
+
 # move to the next color
         if i == len(col_list) - 1:
             i = 0
@@ -76,13 +92,6 @@ for input_file in input_file_list:
 leg = ax.legend(loc='upper left')
 leg.get_frame().set_alpha(0.4)
 # legend display
-print lines
-print arrows_list
-
-# draw()
-#plt.pause(5)
-
-arrows_list[1].set_visible(False)
 
 lined = dict()
 arrowed = dict()
@@ -93,6 +102,7 @@ for legline, origline in zip(leg.get_lines(), lines):
 
 for legline, origarrow in zip(leg.get_lines(), arrows_list):
     arrowed[legline] = origarrow
+
 
 def onpick(event):
     # on the pick event, find the orig line corresponding to the
@@ -117,5 +127,8 @@ def onpick(event):
 fig.canvas.mpl_connect('pick_event', onpick)
 
 plt.axis('equal')
-plt.grid(True)
+plt.grid(b=True, which='major', color='k', linestyle='-', alpha=0.1)
+plt.grid(b=True, which='minor', color='r', linestyle='-', alpha=0.1)
+plt.minorticks_on()
+#plt.grid(b=True, which='minor')#, linestyle='--')
 plt.show()
