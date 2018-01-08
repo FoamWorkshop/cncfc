65d41d9156a579eb5c17d8996054c6de80ac5d36
adam moblap <adam.narozniak@gmail.com>
2018-01-08 07:23:25 +0100

    uncontroled change

--- loft_cut.py
+++ loft_cut.py
@@ -13,7 +13,7 @@ import sys
 import dxfgrabber
 import numpy as np
 from numpy import NaN, pi
-from cncfclib import *
+import cncfclib as cf
 import collections
 from scipy import interpolate
 np.set_printoptions(precision=1)
@@ -23,254 +23,6 @@ from scipy.interpolate import CubicSpline
 
 pt=collections.namedtuple('pt',['x', 'y'])
 
-def angl_conv(x):
-    out = x
-
-    if x<0:
-        out = 2*pi + x
-
-    return out
-
-vangl=np.vectorize(angl_conv)
-
-def dist(P1, P2):
-    return np.sqrt(np.sum(((P1-P2)**2)))
-
-def radius_segment(P1,P2,P3):
-    P4 = np.abs( (P2[:,1]-P1[:,1]) * P3[:,0] - (P2[:,0]-P1[:,0]) * P3[:,1] + P2[:,0]*P1[:,1] - P2[:,1]*P1[:,0]) / np.sqrt( (P2[:,1] - P1[:,1])**2 + (P2[:,0]-P1[:,0])**2 )
-    return np.vstack(P4)
-
-def radius(P1,P2):
-    r= np.sqrt((P1[:,0] - P2[:,0])**2 + (P1[:,1] - P2[:,1])**2)
-    return np.vstack(r)
-
-def angle_segment(P1,P2,P3):
-    '''   P2
-         /
-        /
-       P4.
-      /       .
-    P1             P3
-    '''
-    k = ((P2[:,1]-P1[:,1]) * (P3[:,0]-P1[:,0]) - (P2[:,0]-P1[:,0]) * (P3[:,1]-P1[:,1])) / ((P2[:,1]-P1[:,1])**2 + (P2[:,0]-P1[:,0])**2)
-    P4 = np.vstack([P3[:,0] - k * (P2[:,1]-P1[:,1]), P3[:,1] + k * (P2[:,0]-P1[:,0])]).T
-    angl = np.arctan2(P4[:,1] - P3[:,1], P4[:,0] - P3[:,0])
-    angl = np.vstack(angl)
-    return angl#np.apply_along_axis(lambda x:x if x>=0 else 2*pi+x, 1, angl)
-
-def cross_point(P1,P2,P3):
-    '''   P2
-         /
-        /
-       P4.
-      /       .
-    P1             P3
-    '''
-    k = ((P2[:,1]-P1[:,1]) * (P3[:,0]-P1[:,0]) - (P2[:,0]-P1[:,0]) * (P3[:,1]-P1[:,1])) / ((P2[:,1]-P1[:,1])**2 + (P2[:,0]-P1[:,0])**2)
-    P4 = np.vstack([P3[:,0] - k * (P2[:,1]-P1[:,1]), P3[:,1] + k * (P2[:,0]-P1[:,0])]).T
-
-
-    return P4#np.apply_along_axis(lambda x:x if x>=0 else 2*pi+x, 1, angl)
-
-def angle_test(P1,P2,P3):
-    '''   P3
-         /
-        / angl
-       P2-------P1
-
-    v1 = P1-P2 <- ref. vector
-    v2 = P3-P2
-    '''
-    v1 = P1-P2
-    v2 = P3-P2
-
-    angl1 = np.vstack(np.arctan2(v1[:,1],v1[:,0]))
-    angl2 = np.vstack(np.arctan2(v2[:,1],v2[:,0]))
-    dangl = np.apply_along_axis(lambda x:x if x>=0 else 2*pi+x, 1, angl2-angl1)
-
-    return np.vstack(dangl)
-
-def angle_atan2(P1,P2,P3):
-    '''   P3
-         /
-        / angl
-       P2-------P1
-
-    v1 = P1-P2 <- ref. vector
-    v2 = P3-P2
-    '''
-    v1 = P1-P2
-    v2 = P3-P2
-
-    angl1 = np.vstack(np.arctan2(v1[:,1],v1[:,0]))
-    angl2 = np.vstack(np.arctan2(v2[:,1],v2[:,0]))
-    dangl = np.apply_along_axis(lambda x:x if x>=0 else 2*pi+x, 1, angl2-angl1)
-    dangl = np.apply_along_axis(lambda x:x if x<=pi else x-2*pi, 1, dangl)
-
-    return np.vstack(dangl)
-
-
-def coords2file(name,coords_XU, coords_YV):
-    pref_1='xyuv_'
-    pref_2='r_'
-
-    f = open(name, 'w')
-    for XU, YV in zip(coords_XU,coords_YV):
-        f.write('{0:.3f} {1:.3f}\n'.format(XU, YV))
-
-    f.close()
-
-def Rcoords2file(name,coords_R):
-    pref_1='xyuv_'
-    pref_2='r_'
-
-    f = open(name, 'w')
-    for R in coords_R:
-        f.write('{0:.3f}\n'.format(R))
-
-    f.close()
-
-def list_entities(dxf):
-    dxf_summary = [shape.dxftype for shape in dxf.entities]
-    print('{0:<10}: {1}'.format('LINES', dxf_summary.count('LINE')))
-    print('{0:<10}: {1}'.format('ARCS', dxf_summary.count('ARC')))
-
-def knots_dict(knots_list):
-    return [[i, var] for i, var in enumerate(list(set(knots_list)))]
-
-def interp_points(seg_P1_X_list, seg_P1_Y_list, seg_O_X_list, seg_O_Y_list, C_Z, n_sect, interp_meth, name, layer_list):
-
-
-    P1_X=np.vstack(seg_P1_X_list)
-    P1_Y=np.vstack(seg_P1_Y_list)
-    O_X=np.vstack(seg_O_X_list)
-    O_Y=np.vstack(seg_O_Y_list)
-
-    yv = np.hstack((np.linspace(C_Z[0],C_Z[-1],n_sect),C_Z))
-    yv = np.unique(yv)
-    yv = np.sort(yv)
-    #
-    n_spokes = len(seg_P1_X_list[0])
-    n_sections = len(yv)
-
-    truss_list=[]    # print P1_X
-
-    interp_P1_X_list = []
-    interp_P1_Y_list = []
-    interp_O_X_list = []
-    interp_O_Y_list = []
-    #
-    for i in range(n_spokes):
-
-        interp_P1_X = interpolate.interp1d(C_Z, P1_X[:,i],kind=interp_meth)(yv)
-        interp_P1_Y = interpolate.interp1d(C_Z, P1_Y[:,i],kind=interp_meth)(yv)
-        interp_O_X =  interpolate.interp1d(C_Z, O_X[:,0],kind=interp_meth)(yv)
-        interp_O_Y =  interpolate.interp1d(C_Z, O_Y[:,0],kind=interp_meth)(yv)
-    #
-        interp_P1_X_list.append(interp_P1_X)
-        interp_P1_Y_list.append(interp_P1_Y)
-        interp_O_X_list.append(interp_O_X)
-        interp_O_Y_list.append(interp_O_Y)
-
-    #print np.array([interp_O_X,interp_O_Y]).T
-    #
-    interp_P2_X_list= interp_P1_X_list[1:] + interp_P1_X_list[:1]
-    interp_P2_Y_list= interp_P1_Y_list[1:] + interp_P1_Y_list[:1]
-
-    P1_X = np.vstack([interp_P1_X_list]).T
-    P1_Y = np.vstack([interp_P1_Y_list]).T
-    P2_X = np.vstack([interp_P2_X_list]).T
-    P2_Y = np.vstack([interp_P2_Y_list]).T
-    O_X = np.vstack([interp_O_X_list]).T
-    O_Y = np.vstack([interp_O_Y_list]).T
-
-    C_a = np.ones([n_sections,n_spokes])
-    C_r = np.ones([n_sections,n_spokes])
-
-    P1 = np.vstack([P1_X[:,0], P1_Y[:,0]]).T
-    P2 = np.vstack([P2_X[:,0], P2_Y[:,0]]).T
-    O  = np.vstack([O_X[:,0], O_Y[:,0]]).T
-
-    P_ref = np.ones((len(yv) , 2)) * np.array([[1,0]])
-    C_S = cross_point(P1, P2, O)
-    C_a_ref = angle_test( P_ref, O, C_S) #* 180/pi
-
-    for i in range(n_spokes):
-        print i
-        P1 = np.vstack([P1_X[:,i], P1_Y[:,i]]).T
-        P2 = np.vstack([P2_X[:,i], P2_Y[:,i]]).T
-        O = np.vstack([O_X[:,i], O_Y[:,i]]).T
-        C = cross_point(P1, P2, O)
-        C_r[:,i]=radius(C,O)[:,0]
-        C_a[:,i]=(angle_test( C_S, O, C) + C_a_ref)[:,0]* 180/pi
-
-    # print C_a
-        # C_a[:,i] = C_a[:,i]#+C_a_ref
-    print C_r
-    print C_a
-    # #
-    cut_in_swing = True
-    for i in range(n_spokes):
-
-        f_name_C_r1='{0}_xyuv_{1:{fill}{align}4}.knt'.format(layer_list,i,fill='0',align='>')
-        f_name_C_a1='{0}_b_{1:{fill}{align}4}.knt'.format(layer_list,i,fill='0',align='>')
-
-        if cut_in_swing and i%2:
-            coords2file(f_name_C_r1, np.flipud(C_r[:,i]), np.flipud(yv))
-            Rcoords2file(f_name_C_a1, np.flipud(C_a[:,i]))
-        else:
-            coords2file(f_name_C_r1, C_r[:,i], yv)
-            Rcoords2file(f_name_C_a1, C_a[:,i])
-
-
-def interp_points_fc(seg_P1_X_list, seg_P1_Y_list, C_Z, n_sect, interp_meth, name):
-    print('generate freecad file')
-    n_spokes = len(seg_P1_X_list[0])
-    print(n_spokes)
-
-    P1_X=np.vstack(seg_P1_X_list)
-    P1_Y=np.vstack(seg_P1_Y_list)
-    yv = np.hstack((np.linspace(C_Z[0],C_Z[-1],n_sect),C_Z))
-    yv = np.unique(yv)
-    yv = np.sort(yv)
-
-
-    truss_list=[]
-    print C_Z
-    for i in range(n_spokes):
-
-
-        if interp_meth=='spline':
-            path_P1_X = CubicSpline(C_Z, P1_X[:,i])(yv)
-            path_P1_Y = CubicSpline(C_Z, P1_Y[:,i])(yv)
-
-        else:
-            path_P1_X = interpolate.interp1d(C_Z, P1_X[:,i],kind=interp_meth)(yv)
-            path_P1_Y = interpolate.interp1d(C_Z, P1_Y[:,i],kind=interp_meth)(yv)
-
-
-        truss_list.append(np.vstack([path_P1_X,path_P1_Y,yv]))
-
-    doc = FreeCAD.newDocument()
-    myPart = doc.addObject('Part::Feature', 'trus')
-
-    wire_list_1=[]
-    for truss in truss_list:
-        points_1 = np.transpose(truss)
-        line_list = []
-
-        for p1, p2 in zip(list(points_1[:-1,:]),list(points_1[1:,:])):
-            # print(tuple(p1),tuple(p2))
-            line_list.append(Part.makeLine(tuple(p1), tuple(p2)))
-
-
-        wire_list_1.append(Part.Wire(line_list))
-    wire_list_2= wire_list_1[-1:] + wire_list_1[:-1]
-
-    for w1, w2 in zip(wire_list_1, wire_list_2):
-        myPart = doc.addObject('Part::Feature', 'truss')
-        myPart.Shape = Part.makeRuledSurface(w1,w2)
-    doc.saveAs(name +'.fcstd')
 
 def dxf_read(files, layer_name, tol):
 
@@ -475,10 +227,10 @@ else:
                 S=np.ones((n_spokes , 2)) * np.array(c_set)
                 P=np.ones((n_spokes , 2)) * np.array(pt_P_list)
 
-                r = radius(P, O)
-                a = angle_test( S, O, P)*180/pi
+                r = cf.radius(P, O)
+                a = cf.angle_test( S, O, P)*180/pi
                 P_ref = np.array([[1,0]])
-                a_ref = angle_test( P_ref, O, S) * 180/pi
+                a_ref = cf.angle_test( P_ref, O, S) * 180/pi
                 a_inds = np.argsort(a.T)
                 seg_P1 = P[a_inds][0]
                 seg_P2 = np.roll(seg_P1,-1, axis=0)
@@ -489,51 +241,17 @@ else:
                 seg_O_X_list.append(np.array(pt_O_list[0])[0])
                 seg_O_Y_list.append(np.array(pt_O_list[0])[1])
 
-                C = cross_point(seg_P1, seg_P2, O)
+                C = cf.cross_point(seg_P1, seg_P2, O)
                 C_S = np.ones((n_spokes , 2)) * C[0,:]
-                C_a = angle_test( C_S, O, C) * 180/pi
-                C_a_ref = angle_test( P_ref, O, C_S) * 180/pi
+                C_a = cf.angle_test( C_S, O, C) * 180/pi
+                C_a_ref = cf.angle_test( P_ref, O, C_S) * 180/pi
                 C_r = radius(C,O)
 
 
                 C_r_list.append(C_r.T)
                 C_a_list.append(C_a.T + C_a_ref.T)
 
-            # print seg_P1_list
-            # print(np.vstack(seg_P1_X_list))
-            # print(np.vstack(seg_P1_Y_list))
-
-            interp_points(seg_P1_X_list, seg_P1_Y_list , seg_O_X_list, seg_O_Y_list, C_Z, n_sect, interp_meth,files_dxf_member,layer_list)
-
-            interp_points_fc(seg_P1_X_list, seg_P1_Y_list, C_Z, n_sect, interp_meth,files_dxf_member)
-            # C_a1=np.vstack(C_a_list)
-            # # C_a2=np.roll(C_a1,-1, axis=0)
-            #
-            # C_r1=np.vstack(C_r_list)
-            # # C_r2=np.roll(C_r1,-1, axis=0)
-            #
-            # yv = np.hstack((np.linspace(C_Z[0],C_Z[-1],n_sect),C_Z))
-            # yv = np.unique(yv)
-            # yv = np.sort(yv)
-            #
-            # print n_spokes
-            # cut_in_swing = True
-            # for i in range(n_spokes):
-            #     print len(C_Z)
-            #     print len(C_r1[:,i])
-            #     path_C_r1 = interpolate.interp1d(C_Z, C_r1[:,i],kind=interp_meth)(yv)
-            #     path_C_a1 = interpolate.interp1d(C_Z, C_a1[:,i],kind=interp_meth)(yv)
-            #     f_name_C_r1='{0}_xyuv_{1:{fill}{align}4}.knt'.format(layer_list,i,fill='0',align='>')
-            #     f_name_C_a1='{0}_b_{1:{fill}{align}4}.knt'.format(layer_list,i,fill='0',align='>')
-            #     print('yv: {}'.format(yv))
-            #     print('r:  {}'.format(path_C_r1))
-            #     print('a:  {}'.format(path_C_a1))
-            #
-            #     if cut_in_swing and i%2:
-            #         coords2file(f_name_C_r1, np.flipud(path_C_r1), np.flipud(yv))
-            #         Rcoords2file(f_name_C_a1, np.flipud(path_C_a1))
-            #     else:
-            #         coords2file(f_name_C_r1, path_C_r1, yv)
-            #         Rcoords2file(f_name_C_a1, path_C_a1)
+            cf.interp_points(seg_P1_X_list, seg_P1_Y_list , seg_O_X_list, seg_O_Y_list, C_Z, n_sect, interp_meth,files_dxf_member,layer_list)
+            cf.interp_points_fc(seg_P1_X_list, seg_P1_Y_list, C_Z, n_sect, interp_meth,files_dxf_member)
 
 print "\n end of program. thank you!"
--- stl2section_cyl.py
+++ stl2section_cyl.py
@@ -8,8 +8,24 @@ mesh.rotate([0,1,0],np.pi/2)
 v_arr = np.round(np.vstack(mesh.vectors).astype(float), decimals=1)
 pos = cf.cartesian2cylyndrical(v_arr,17)
 
-spars=np.zeros_like(pos)
+profiles=np.zeros_like(pos)
 for i in np.arange(np.shape(pos)[0]):
-    spars[i] = cf.cylyndrical2cartesian(pos[i])
+    profiles[i] = cf.cylyndrical2cartesian(pos[i])
 
-cf.plot_loft_paths(spars)
+# cf.plot_loft_paths(profiles)
+
+# print(profiles)
+
+strokes = np.flipud(np.rot90(profiles))
+# print(strokes)
+# data=np.zeros_like(profiles)
+
+# for i in np.arange(np.shape(profiles)[1]):
+#     strokes[i]=profiles[:,i]
+#     # ax.plot(spars[:,0],spars[:,1],spars[:,2],'x-')
+#     ax.plot(data[:,i,0],data[:,i,1],data[:,i,2],'x-')
+#
+# print(profiles)
+# b=np.flipud(np.rot90(a))
+
+cf.interp_points_cyl(strokes,'sdsd','sadada')
