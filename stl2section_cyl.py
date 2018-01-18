9c9fe1b52a10c629a401f5d374df597ba2a7d6b4
adam moblap <adam.narozniak@gmail.com>
2018-01-18 07:03:23 +0100

    removed the the main function structure, added surf plotting of transformed parameters

--- stl2section_cyl.py
+++ stl2section_cyl.py
@@ -2,6 +2,12 @@ import numpy as np
 from stl import mesh
 import cncfclib as cf
 
+# def main():
+"""
+This program converts stl files to paths for CNCFoamCutter.
+slender models eg. fusselage modeled in OpenVSP.
+"""
+
 mesh = mesh.Mesh.from_file('fuselage.stl')
 mesh.rotate([0,1,0],np.pi/2)
 
@@ -14,25 +20,24 @@ profiles=np.zeros_like(pos)
 for i in np.arange(np.shape(pos)[0]):
     profiles[i] = cf.cylyndrical2cartesian(pos[i])
 
-cf.plot_loft_paths(profiles)
-# cf.plot_loft_paths(pos)
+    # cf.plot_loft_paths(profiles)
+    # cf.plot_loft_paths(pos)
 
 strokes = np.flipud(np.rot90(profiles))
+# cf.plot_loft_paths(strokes)
+# print(strokes)
 ang_arr, r_arr, z_arr, v_arr = cf.transform(strokes, add_pedestal_bottom=True,add_pedestal_top=True)
+cf.plot_surf(ang_arr,z_arr,r_arr)
 
-print('ang', ang_arr)
-print('r', r_arr)
-print('z', z_arr)
-print('v', v_arr)
-#
-# np.savetxt('r_arr',r_arr)
-# np.savetxt('a_arr',ang_arr)
-# np.savetxt('z_arr',z_arr)
-# np.savetxt('v_arr',v_arr)
-# add_pedestal()
 
-# buff_2v.tofile('ndarray_test.dat',sep=";")
-# np.save('ndarray_test.dat', buff_2v)
-# k=np.load('ndarray_test.dat.npy', mmap_mode='r')
-# print(k)
+res_dict = {'a_arr':ang_arr,
+            'r_arr':r_arr,
+            'z_arr':z_arr,
+            'v_arr': v_arr}
+
+np.save('res_dict.npy', res_dict)
+
 print('saved')
+
+# if __name__ == "__main__":
+#     main()
